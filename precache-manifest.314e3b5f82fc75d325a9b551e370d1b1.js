d304ef06a87ba0fb388c761be24a4bd7843fb6e1
Shyraine Borres <shyraineborres@Shyraines-MacBook-Air.local>
2024-03-24 19:58:01 +0800

    Updates

---
+++ precache-manifest.314e3b5f82fc75d325a9b551e370d1b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3def8521b5176c086d54",
+    "url": "/shyraineborres.github.io/static/css/main.279aea94.chunk.css"
+  },
+  {
+    "revision": "3def8521b5176c086d54",
+    "url": "/shyraineborres.github.io/static/js/main.3def8521.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/shyraineborres.github.io/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "3c8e7bf05121a9dd8c60",
+    "url": "/shyraineborres.github.io/static/js/runtime~main.3c8e7bf0.js"
+  },
+  {
+    "revision": "4217b2f7edcf6062c424f3dc5be02541",
+    "url": "/shyraineborres.github.io/index.html"
+  }
+];
